7c6543258cac41ce221113e56303fedc2b713326
utkusrtlk <usertlek2006@yahoo.com>
2023-12-20 14:33:25 +0300

    first commit

---
+++ MyFirstProject/src/deneme/HelloJava.java
@@ -0,0 +1,11 @@
+package deneme;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// merhaba yazan program
+		System.out.println("Hello Java");
+		System.out.print("This is Utku");
+	}
+
+}
